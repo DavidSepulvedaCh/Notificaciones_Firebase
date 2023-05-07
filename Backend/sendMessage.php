3ee3b194fdc0723bf158238743050d8dcdc2320c
David Leonardo Sepúlveda Chávez <116600515+DavidSepulvedaCh@users.noreply.github.com>
2023-05-07 00:07:11 -0500

    Envió de notificaciones Backend
    Servicio de envió de mensajes según el id a quien va dirigido hace la consulta en la BD y extrae el token para enviar el mensaje.

--- Backend/sendMessage.php
+++ Backend/sendMessage.php
@@ -1,57 +1,47 @@
 <?php
-
 include_once 'headers.php';
 require __DIR__ . '/vendor/autoload.php';
-
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-class SendMessage
+class SendMenssage
 {
     public $code = "";
     public $message = "";
+
 }
 
-$resp = new SendMessage;
+$resp = new SendMenssage;
 $resp->code = "Ok";
-$resp->message = "Message send!";
+$resp->message = "Mensaje enviado exitosamente";
 $tokenUser = [];
 
-
-
 $mysqli = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
-
 if ($mysqli->connect_error) {
     $resp->code = "Error";
-    $resp->message = "Error en la conexión con la  BD: " . $mysqli->connect_error;
+    $resp->message = ("Error al conectar a la base de datos: " . $mysqli->connect_error);
 }
 
-/* $users = [];
-$consulta = mysqli_query($mysqli, "SELECT * FROM usuarios");
-while ($row = mysqli_fetch_assoc($consulta)) {
-array_push($users, $row);
-} */
+if (!empty($_POST['EMAIL_REM']) && !empty($_POST['EMAIL_RES']) && !empty($_POST['TITLE']) && !empty($_POST['MESSAGE']) && !empty($_POST['ID_RES'])) {
+    $emailRem = $_POST['EMAIL_REM'];
+    $emailRes = $_POST['EMAIL_RES'];
+    $idRes = $_POST['ID_RES'];
+    $title = $_POST['TITLE'];
+    $body = $_POST['MESSAGE'];
 
 
-//obteniedo el token del destinatario
-if (!empty($_POST['id_user']) && !empty($_POST['titulo']) && !empty($_POST['mensaje'])) {
-    $id = $_POST['id_user'];
     $consulta = $mysqli->prepare("SELECT token_fcm FROM dispositivos WHERE usuario_id = ?");
-    $consulta->bind_param("i", $id);
+    $consulta->bind_param("i", $idRes);
     $consulta->execute();
-    $result = $consulta->get_result();
-    while ($row = $result->fetch_assoc()) {
+    $rta = $consulta->get_result();
+    while ($row = $rta->fetch_assoc()) {
         $tokenUser[] = $row['token_fcm'];
     }
     $consulta->close();
-    $mysqli->close();
 
-
-    $titulo = $_POST['titulo'];
-    $mensaje = $_POST['mensaje'];
     $notification = [
-        "title" => $titulo,
-        "body" => $mensaje
+        "title" => $title,
+        "body" => $body
     ];
 
     $fields = [
@@ -65,6 +55,7 @@ if (!empty($_POST['id_user']) && !empty($_POST['titulo']) && !empty($_POST['mens
         "authorization: key=" . $_ENV['FCM_API'],
         "content-type: application/json"
     ];
+
     $ch = curl_init();
     curl_setopt_array($ch, [
         CURLOPT_URL => $url,
@@ -76,17 +67,25 @@ if (!empty($_POST['id_user']) && !empty($_POST['titulo']) && !empty($_POST['mens
     ]);
     $result = curl_exec($ch);
     curl_close($ch);
-
     if ($result === false) {
         $resp->code = "ERR";
-        $resp->message = "Error";
+        $resp->message = "Error al enviar el mensaje, intentalo de nuevo";
     } else {
         $resp->code = "OK";
         $resp->message = "El mensaje se envio exitosamente";
+        $msm = $mysqli->prepare("INSERT INTO mensajes (titulo, cuerpo, remitente_email, destinatario_email, 
+            destinatario_dispositivo_id, resultado_notificacion_push) VALUES (?, ?, ?, ?, ?, ?)");
+        $msm->bind_param("ssssis", $title, $body, $emailRem, $emailRes, $idRes, $resp->code);
+        $msm->execute();
+        $msm->close();
+
     }
+
 } else {
     $resp->code = "error";
     $resp->message = "No se envio el mensaje, perdida de datos.";
 }
 echo json_encode($resp);
+?>
+
 ?>
